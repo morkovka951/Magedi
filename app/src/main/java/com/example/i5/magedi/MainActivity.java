948114b170d8bd52e3a472a20381390d6a801dee
Morkovka <mhdd@ukr.net>
2017-05-03 00:02:51 +0300

    second commit (add activity2)

--- app/src/main/java/com/example/i5/magedi/MainActivity.java
+++ app/src/main/java/com/example/i5/magedi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.i5.magedi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +20,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-
+        switch (view.getId()){
+            case R.id.btnMain:
+                Intent intent = new Intent(this, Activity2.class);
+                startActivity(intent);
+                finish();
+        }
     }
 }
