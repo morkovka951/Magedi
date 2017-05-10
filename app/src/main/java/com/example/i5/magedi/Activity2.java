7369acc2865e667ec14c6e7540043fc368f691f2
Morkovka <mhdd@ukr.net>
2017-05-10 22:56:05 +0300

    add activity3+spinner

--- app/src/main/java/com/example/i5/magedi/Activity2.java
+++ app/src/main/java/com/example/i5/magedi/Activity2.java
@@ -1,13 +1,22 @@
 package com.example.i5.magedi;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+        import com.hbb20.CountryCodePicker;
 
 public class Activity2 extends AppCompatActivity {
+
+    CountryCodePicker ccp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        ccp = (CountryCodePicker) findViewById(R.id.ccp);
+
+
     }
+
 }
--- app/src/main/java/com/example/i5/magedi/MainActivity.java
+++ app/src/main/java/com/example/i5/magedi/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.i5.magedi;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
