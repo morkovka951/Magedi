b4d8fa5c8e2be26233d55f4a313429eef0f60cf0
Morkovka <mhdd@ukr.net>
2017-05-14 23:29:12 +0300

    NOT UPD
    +country code NOT UPD

--- app/src/main/java/com/example/i5/magedi/Activity2.java
+++ app/src/main/java/com/example/i5/magedi/Activity2.java
@@ -2,20 +2,31 @@ package com.example.i5.magedi;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.widget.ArrayAdapter;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.Spinner;
 
         import com.hbb20.CountryCodePicker;
 
 public class Activity2 extends AppCompatActivity {
 
-    CountryCodePicker ccp;
+    Spinner spinner;
+    EditText editText2;
+    Button btnActiv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
-        ccp = (CountryCodePicker) findViewById(R.id.ccp);
-        int e = ccp.getSelectedCountryCodeAsInt();
+        spinner = (Spinner) findViewById(R.id.spinner);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        btnActiv2 = (Button) findViewById(R.id.btnActiv2);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
     }
 
